ef180a957f925ec58caf2ea68d47801555ca0c26
Nicholas Kolatsis <nkolatsis@users.noreply.github.com>
2017-11-22 15:34:07 +0100

    LearnYouMongo Problem 3 - FIND, solved.

---
+++ 03. FIND/program.js
@@ -0,0 +1,12 @@
+var mongo = require("mongodb").MongoClient
+
+
+mongo.connect("mongodb://localhost:27017/learnyoumongo", function(err, db) {
+    db.collection("parrots")
+        .find({age: {$gt: parseInt(process.argv[2])}})
+        .toArray(function (err, documents){
+            if (err) console.error(err)
+            console.log(documents)
+        })
+    db.close()
+})
